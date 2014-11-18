aeb974a39cec62e2462e8322ff7c685846c65672
Xryaznov <xryaznov@gmail.com>
2014-11-18 21:12:28 +0200

    ready to deploy

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String webappDirLocation = "src/main/webapp";
+        String webappDirLocation = "/webapp";
         Tomcat tomcat = new Tomcat();
         String webPort = System.getenv("PORT");
         if (webPort == null || webPort.isEmpty()) {
